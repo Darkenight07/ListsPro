5aca5b2dc03cec2aecf2154476c4bab96e29da17
Darkenight07 <samueldynamo11@gmail.com>
2023-11-27 15:27:57 +0100

    Creada la clase para gestionar las listas

---
+++ src/com/darkenight07/Listas/GestionListas.java
@@ -0,0 +1,21 @@
+package com.darkenight07.Listas;
+public class GestionListas {
+
+    public GestionListas() {
+
+    }
+
+    public void crearLista(String nombreLista) {
+
+    }
+
+    public void editarLista(String nombreLista) {
+
+    }
+
+    public void eliminarLista(String nombreLista) {
+
+    }
+
+
+}
